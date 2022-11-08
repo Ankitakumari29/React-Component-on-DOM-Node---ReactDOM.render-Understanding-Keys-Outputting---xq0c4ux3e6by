1b69fbc85c214a04b6aa5624ba9922f1a6f748de
Ankitakumari29 <111568046+Ankitakumari29@users.noreply.github.com>
2022-11-08 22:48:06 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
+import { render } from "enzyme";
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
 
-
-
+ReactDOM.render(<App />, document.getElementById("root"));
